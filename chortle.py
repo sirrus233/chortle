40e91fc68c4b25bc8001b8d4f82d1c3938407278
Bradley Sherman <sirrus233@gmail.com>
2018-10-07 01:40:27 -0700

    Periodically scan the model database and update statuses based on last button press time. Use threads to do this concurrently with handling button press events

--- chortle.py
+++ chortle.py
@@ -2,26 +2,52 @@
 
 import boto3
 import json
+import time
 from datetime import datetime
+from threading import Thread
+
+
+class UpdateChoreStatus(Thread):
+    def run(self):
+        dynamo = boto3.resource('dynamodb')
+        table = dynamo.Table('chortle-model')
+
+        while(True):
+            for item in table.scan()['Items']:
+                current_time = datetime.now()
+                last_pressed_time = datetime.fromisoformat(item['last_pressed_time'])
+                time_diff_seconds = (current_time - last_pressed_time).seconds
+                status_ok = time_diff_seconds < item['reset_time_seconds']
+                if status_ok != item['status_ok']:
+                    table.update_item(
+                            Key={'button_serial': item['button_serial']},
+                            UpdateExpression='SET status_ok=:s',
+                            ExpressionAttributeValues={':s': status_ok})
+            time.sleep(2)
+
+
+class ProcessButtonPresses(Thread):
+    def run(self):
+        sqs = boto3.resource('sqs')
+        queue = sqs.get_queue_by_name(QueueName='chortle')
+
+        dynamo = boto3.resource('dynamodb')
+        table = dynamo.Table('chortle-model')
+
+        while(True):
+            messages = queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=20)
+            for message in messages:
+                message_json = json.loads(message.body)
+                button_serial = message_json['Message']
+
+                table.update_item(
+                        Key={'button_serial': button_serial},
+                        UpdateExpression='SET last_pressed_time=:t',
+                        ExpressionAttributeValues={':t': datetime.isoformat(datetime.now(), timespec='seconds')})
+
+                message.delete()
 
 
 if __name__ == '__main__':
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName='chortle')
-
-    dynamo = boto3.resource('dynamodb')
-    table = dynamo.Table('chortle-model')
-
-    while(True):
-         messages = queue.receive_messages(MaxNumberOfMessages=10, WaitTimeSeconds=20)
-         for message in messages:
-             message_json = json.loads(message.body)
-             button_serial = message_json['Message']
-
-             #table.get_item(Key={'button_serial': button_serial})
-             table.update_item(
-                     Key={'button_serial': button_serial},
-                     UpdateExpression='SET last_pressed_time=:t',
-                     ExpressionAttributeValues={':t': datetime.isoformat(datetime.now(), timespec='seconds')})
-
-             message.delete()
+    ProcessButtonPresses().start()
+    UpdateChoreStatus().start()
