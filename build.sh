9b55c753b51f3f4a9862826902f5f7c16211c144
Bradley Sherman <sirrus233@gmail.com>
2018-10-07 20:50:50 -0700

    Refactored out into files that can be directly uploaded to lambda, which now does all the work instead of a host on our end. Also added a build script to upload files to lambda.

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+src_dir=$(dirname "$0")/src
+lambda_dir=${src_dir}/lambda
+build_dir=$(dirname "$0")/build
+
+lambda_functions=('chortle-button-press' 'chortle-status-update')
+
+mkdir -p $build_dir
+
+for target in ${lambda_functions[@]}; do
+    artifact=${build_dir}/${target}.zip
+    zip -rj $artifact ${lambda_dir}/${target}
+    aws lambda update-function-code --profile bradley --function-name $target --zip-file fileb://${artifact}
+done
---
+++ src/lambda/chortle-button-press/lambda_function.py
@@ -0,0 +1,23 @@
+import os
+import boto3
+import time
+
+CHORTLE_DYNAMO_TABLE = os.environ['CHORTLE_DYNAMO_TABLE'] 
+
+def lambda_handler(event, context):
+    # Deserialize button event JSON
+    serial_number = event['serialNumber']
+    click_type = event['clickType']
+    battery_voltage = event['batteryVoltage']
+    print('Received button press from SN:{} with clickType {} and voltage {}'.format(
+        serial_number, click_type, battery_voltage))
+    
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(CHORTLE_DYNAMO_TABLE)
+    
+    current_time = int(time.time())
+    key = {'button_serial': serial_number}
+    expr = 'SET last_pressed_time=:t'
+    expr_attrs = {':t': current_time}
+    table.update_item(Key=key, UpdateExpression=expr, ExpressionAttributeValues=expr_attrs)
+    print('Table updated with new timestamp {}'.format(current_time))
