a553d4e83a7effe997912f3a24898ab9cfce184a
Bradley Sherman <sirrus233@gmail.com>
2018-10-16 02:15:44 -0700

    Refactor the strategy functions, and add support for 'modal ' chores, which have dependents

--- src/lambda/chortle-button-press/lambda_function.py
+++ src/lambda/chortle-button-press/lambda_function.py
@@ -14,16 +14,18 @@ def lambda_handler(event, context):
     battery_voltage = event['batteryVoltage']
     print('Received button press from SN:{} with clickType {} and voltage {}'.format(
         serial_number, click_type, battery_voltage))
-    
+
+    # Get a reference to the database table
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(CHORTLE_DYNAMO_TABLE)
-    key = {'button_serial': serial_number, 'click_type': click_type}
 
-    entry = table.get_item(Key=key)['Item']
+    # Query the chore entry corresponding to the button that was pressed
+    event_key = {'button_serial': serial_number, 'click_type': click_type}
+    entry = table.get_item(Key=event_key)['Item']
     print('Retrieved entry from table: {}'.format(entry))
 
     try:
-        expr, expr_attrs = get_update_expression(entry)
-        table.update_item(Key=key, UpdateExpression=expr, ExpressionAttributeValues=expr_attrs)
+        for key, expr, expr_attrs in get_update_expression(entry):
+            table.update_item(Key=key, UpdateExpression=expr, ExpressionAttributeValues=expr_attrs)
     except UnknownStrategyException:
         print('Unknown strategy: {}'.format(entry['strategy']))
--- src/lambda/chortle-button-press/strategy_handler.py
+++ src/lambda/chortle-button-press/strategy_handler.py
@@ -5,32 +5,43 @@ class UnknownStrategyException(Exception):
     pass
 
 
-def __get_periodic_update_expression(entry):
+def __update_entry_time(entry):
     current_time = int(time.time())
+    key = {'button_serial': entry['button_serial'], 'click_type': entry['click_type']}
     expr = 'SET last_pressed_time=:t'
     expr_attrs = {':t': current_time}
-    return expr, expr_attrs
+    return key, expr, expr_attrs
+
+
+def __get_periodic_update_expression(entry):
+    update_expression = __update_entry_time(entry)
+    return [update_expression]
 
 
 def __get_toggle_update_expression(entry):
-    current_time = int(time.time())
-    active = not entry['active']
-    expr = 'SET last_pressed_time=:t, active=:a'
-    expr_attrs = {':t': current_time, ':a': active}
-    return expr, expr_attrs
+    key, expr, expr_attrs = __update_entry_time(entry)
+    expr += ', active=:a'
+    expr_attrs[':a'] = not entry['active']
+    update_expression = (key, expr, expr_attrs)
+    return [update_expression]
 
 
-def __get_modal_update_expression(entry):
-    current_time = int(time.time())
-    expr = 'SET last_pressed_time=:t'
-    expr_attrs = {':t': current_time}
-    return expr, expr_attrs
+def __get_modal_toggle_update_expression(entry):
+    parent_update_expression = __get_toggle_update_expression(entry)
+    dependent_update_expression = []
+    # Only update the dependent chore if this chore was inactive and is now toggling to active
+    if not entry['active']:
+        dependent_key = {'button_serial': entry['dependent'][0], 'click_type': entry['dependent'][1]}
+        dependent_expr = 'SET active=:a'
+        dependent_expr_attrs = {':a': False}
+        dependent_update_expression.append((dependent_key, dependent_expr, dependent_expr_attrs))
+    return parent_update_expression + dependent_update_expression
 
 
 STRATEGY_FUNCTIONS = {
     'PERIODIC': __get_periodic_update_expression,
     'TOGGLE':   __get_toggle_update_expression,
-    'MODAL':    __get_modal_update_expression
+    'MODAL_TOGGLE':    __get_modal_toggle_update_expression
 }
 
 
